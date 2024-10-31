d271c414e1d1bfd0c6c7beab29881fcacacc6ff9
Илья Павлов <ilyapav@icloud.com>
2024-10-31 10:51:50 +0300

    Initial Commit

---
+++ NetworkTrainigFile/Service/APIEnums.swift
@@ -0,0 +1,8 @@
+//
+//  APIEnums.swift
+//  NetworkTrainigFile
+//
+//  Created by Илья Павлов on 31.10.2024.
+//
+
+
---
+++ NetworkTrainigFile/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NetworkTrainigFile
+//
+//  Created by Илья Павлов on 31.10.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .systemGray6
+    }
+
+
+}
+
